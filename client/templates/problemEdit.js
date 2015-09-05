ad07f0c3252447720b6e029d980d823fdecc65ed
Miloas <0x9357@gmail.com>
2015-09-05 23:02:49 +0800

    add editproblempage

--- client/templates/problemEdit.js
+++ client/templates/problemEdit.js
@@ -1,10 +1,10 @@
 /**
  * Created by miloas on 2015/9/5.
  */
-Template.problemSubmit.events({
-    'submit form': function(e){
+Template.problemEdit.events({
+    'submit form': function (e) {
         e.preventDefault();
-        var problem = {
+        var newProblem = {
             title:$(e.target).find('[name=title]').val(),
             cpu:$(e.target).find('[name=cpu]').val(),
             memory:$(e.target).find('[name=memory]').val(),
@@ -13,12 +13,24 @@ Template.problemSubmit.events({
             outputspec:$(e.target).find('[name=outputspec]').val(),
             sampleinput:$(e.target).find('[name=sampleinput]').val(),
             sampleoutput:$(e.target).find('[name=sampleoutput]').val(),
-            pid:Problems.find({}).count()+1000,
-            submitnum:0,
-            acnum:0,
+            pid:this.pid,
+            submitnum:this.submitnum,
+            acnum:this.acnum,
             hidden:document.getElementById('hidden').checked
         };
-        problem._id = Problems.insert(problem);
-        Router.go('problems');
+        Problems.update(this._id,{$set: newProblem}, function (error) {
+            if(error){
+                alert(error.reason);
+            }else {
+                Router.go('problems');
+            }
+        })
+    },
+    'click .negative': function (e) {
+        e.preventDefault();
+        if(confirm("Delete this problem?")){
+            Problems.remove(this._id);
+            Router.go('problems');
+        }
     }
 })
